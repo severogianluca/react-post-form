942af5cd9b08b961093c922b5e4fe1bcd6c18b3c
severogianluca <gianlucasevero300@gmail.com>
2025-04-14 17:52:43 +0200

    Feat: create form + input & function prevent default

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
+import { useState } from "react";
+
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    author: '',
+    title: '',
+    body: '',
+    public: true
+  });
+  
+  
+  function handleFormdData(e) {
+    const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+  
+    setFormData((formData) => ({
+    ...formData, [e.target.name] : value
+    }));
+
+    }
+    console.log(formData)
+
+    function sendForm(e){
+      e.preventDefault()
+    }
+
+
+
+    
 
   return (
     <>
 
+    <form onSubmit={sendForm}>
+    <input type="text" 
+    name="author"
+    value={formData.name}
+    onChange={handleFormdData}/>
+
+
+    {/* <input type="text" />
+    <input type="text" /> */}
+    <input type="checkbox" 
+    name="public"
+    checked={formData.public}
+    onChange={handleFormdData}/>
+    <button>invio</button>
+
+    </form>
     </>
   )
 }
