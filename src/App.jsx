06e926ba0a1d9f71dff17ed4e22adfe24af90d3a
severogianluca <gianlucasevero300@gmail.com>
2025-04-14 18:17:49 +0200

    Feat: add alert

--- src/App.jsx
+++ src/App.jsx
@@ -10,50 +10,67 @@ function App() {
     body: '',
     public: true
   });
-  
-  
+
+
   function handleFormdData(e) {
     const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
-  
+
     setFormData((formData) => ({
-    ...formData, [e.target.name] : value
+      ...formData, [e.target.name]: value
     }));
 
-    }
-    console.log(formData)
+  }
+  console.log(formData)
 
-    function sendForm(e){
-      e.preventDefault()
-        axios.post("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts", formData)
-        .then((response) => {
-          console.log(response.data)
-        });
-      
-    }
+  function sendForm(e) {
+    e.preventDefault()
+    axios.post("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts", formData)
+      .then((response) => {
+        console.log(response.data)
+        alert("Form inserito con successo")
+      })
+      .catch((error) => {
+        console.error(error)
+        alert("Errore durante l'invio del form")
+      })
+
+  }
 
 
 
-    
+
 
   return (
     <>
 
-    <form onSubmit={sendForm}>
-    <input type="text" 
-    name="author"
-    value={formData.name}
-    onChange={handleFormdData}/>
+      <form onSubmit={sendForm}>
+        <input type="text"
+          name="author"
+          value={formData.name}
+          placeholder="Inserisci autore"
+          onChange={handleFormdData} />
 
 
-    {/* <input type="text" />
-    <input type="text" /> */}
-    <input type="checkbox" 
-    name="public"
-    checked={formData.public}
-    onChange={handleFormdData}/>
-    <button>invio</button>
+        <input type="text"
+          name="title"
+          value={formData.name}
+          placeholder="Inserisci titolo"
+          onChange={handleFormdData} />
 
-    </form>
+        <input type="text"
+          name="body"
+          value={formData.name}
+          placeholder="Inserisci testo"
+          onChange={handleFormdData} />
+
+
+        <input type="checkbox"
+          name="public"
+          checked={formData.public}
+          onChange={handleFormdData} />
+        <button>invio</button>
+
+      </form>
     </>
   )
 }
